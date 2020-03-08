70debacc7e9b641704228e506d6068df0b271516
xxanctuarii <xxanctuarii@icloud.com>
2020-03-08 15:29:51 +0100

    Create screenresize_buttons.js

---
+++ screenresize_buttons.js
@@ -0,0 +1,52 @@
+function toggleFullscreenButtons(){
+	if(window.innerHeight == screen.height){
+	  $('.open').addClass( "hide" );
+	  $('.close').removeClass( "hide" );
+	}
+	else{
+	  $('.open').removeClass( "hide" );
+	  $('.close').addClass( "hide" );
+	}
+}
+
+toggleFullscreenButtons();
+
+var resizeId;
+$(window).resize(function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 500);
+});
+
+function doneResizing(){
+    toggleFullscreenButtons();
+}
+
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
