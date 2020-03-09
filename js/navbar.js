fbf4e9f310f26a301c5eef15a2acb11623189cd0
Dorsten <david-van.dorsten@klm.com>
2020-03-09 10:02:32 +0100

    Added fullscreen buttons

--- js/navbar.js
+++ js/navbar.js
@@ -12,3 +12,63 @@ function activateSidebar(action) {
     break;
   }
 }
+
+/*
+	Fullscreen Buttons
+*/
+
+// Func, based on screensize show appropriate button
+function toggleFullscreenButtons(){
+	if(window.innerHeight == screen.height){
+	  $('.open').addClass( "hide" );
+	  $('.close').removeClass( "hide" );
+	}
+	else{
+	  $('.open').removeClass( "hide" );
+	  $('.close').addClass( "hide" );
+	}
+}
+
+// Call function at page load
+toggleFullscreenButtons();
+
+// Call function after screen resize
+var resizeId;
+$(window).resize(function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 300);
+});
+
+function doneResizing(){
+    toggleFullscreenButtons();
+}
+
+// Functions to activate or deactivate fullscreen
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
